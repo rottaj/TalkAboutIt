43de7fe46accad3a14c7eb8b1e67b19b45bca951
github username <github email address>
2019-03-25 17:13:29 -0500

    adding restful routes to badges

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,2 +1,36 @@
 class BadgesController < ApplicationController
+
+	def index
+		@badges = Badge.all
+	end
+
+	def show
+		@badge = Badge.find(params[:id])
+	end
+
+	def new
+		@badge = Badge.new
+	end
+
+	def create
+		@badge = Badge.new(badge_params)
+		if @badge.valid?
+			@badge.save
+			redirect_to @badge
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@badge = Badge.find(params[:id])
+	end
+
+	def update
+
+	end
+
+	def badge_params
+		params.require(:badge).permit(:name)
+	end
 end
