abf55ddc180a61ee98c54108d6759ca7811990b8
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-25 17:00:54 -0500

    bs with category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,36 @@
 class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.all
+	end
+
+	def show
+		@category = Category.find(params[:id])
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params(:name))
+		if @category.valid?
+			@category.save
+			redirect_to @category
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@category = Category.find(params[:id])
+	end
+
+	def update
+
+	end
+
+	def category_params(*args)
+		params.require(:category).permit(*args)
+	end
 end
