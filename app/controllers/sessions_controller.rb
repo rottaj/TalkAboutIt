44b6c548ef5b1f644b698c8c035fba436478a150
github username <github email address>
2019-03-26 15:07:34 -0500

    everything works, moving on to email stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(user_name: params[:user_name])
+        user = User.find_by_user_name(params[:user_name])
+        session[:user_id] = user.id
 
         if user && user.authenticate(params[:password])
             redirect_to user_path(user)
