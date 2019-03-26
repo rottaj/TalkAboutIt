479698eac818ed2e5704dc5709405111ac8dff15
github username <github email address>
2019-03-26 08:53:44 -0500

    fixed changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,13 @@ class User < ApplicationRecord
     has_many :userbadges
     has_many :badges, through: :userbadges
 
-<<<<<<< HEAD
     def verification
    		
     end
-=======
     #validations
     validates :user_name, uniqueness: true
     validates :email, uniqueness: true
     validates :bio, length: { maximum: 200 }
 
 
->>>>>>> d1207acb82863e1739702b507820aa6d079df8ff
 end
