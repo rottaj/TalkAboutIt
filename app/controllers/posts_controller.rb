d1207acb82863e1739702b507820aa6d079df8ff
github username <github email address>
2019-03-25 19:46:51 -0500

    post/index now sorts by the time created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
     
     def index
-        @posts = Post.all 
+        @posts = Post.sort_posts_by_time 
     end
 
     def show 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
     belongs_to :user 
     has_many :post_categories 
     has_many :categories, through: :post_categories
+
+
+    def self.sort_posts_by_time
+        sorted_posts = Post.all.sort_by { |post| post.created_at }.reverse
+    end
 end
