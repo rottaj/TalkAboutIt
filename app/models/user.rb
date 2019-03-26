03fc21a3b5eba340f0c195cedbadb0d05b8d62f1
github username <github email address>
2019-03-25 19:15:49 -0500

    added validations to badge and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     has_many :posts
     has_many :userbadges
     has_many :badges, through: :userbadges
+
+    #validations
+    validates :user_name, uniqueness: true
+    validates :email, uniqueness: true
+    validates :bio, length: { maximum: 200 }
+
+
 end
