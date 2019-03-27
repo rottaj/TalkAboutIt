c27e6de42c3392fa8b345888ecae38109adaa414
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-27 16:25:39 -0500

    added some css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
     
     def index
         @posts = Post.sort_posts_by_time 
+        @categories = Category.all
     end
 
     def show 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,29 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    after_create :assign_verification_code
+    #after_create :assign_verification_code
 
     has_many :posts
-    
 
-    validates :user_name, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    validates :bio, length: { maximum: 200 }
+    validates :user_name, presence: true
+    #validates :email, presence: true, uniqueness: true
+    #validates :bio, length: { maximum: 200 }
 
 
 	def sendMail
    		mail = Mail.new do
-     		from 'uberswaggerauthenication@gmail.swag'
+     		from 'talkaboutit@verify.mail'
      		to $user.email
-     		subject $user.verification_code
-     		body 'swagger'
+     		subject 'TalkAboutIt Email Verification'
+     		body "Hi #{$user.user_name}! Here is your Verfication Code: #{$user.verification_code}. Thanks for joining TalkAboutIt!"
    		end
     
    		mail.delivery_method :sendmail
    		mail.deliver
+  end
+
+  def is_verified
+    $user.verification_code == $input ? ($user.save && $user.verifcation_code = "True") : (puts "Check your email!")
   end
 
   def assign_verification_code
